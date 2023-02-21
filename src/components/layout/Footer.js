b9a8e6ddfeb1f053a7e7ff55f10bec4dd91de667
wilku1211 <wilku1211c@gmail.com>
2023-02-21 10:37:53 +0100

    Footer added

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,41 @@
 import React from "react";
-
+import classes from "./Footer.module.scss";
+import Fb from "../../assets/image/svg/Fb";
+import Instagram from "../../assets/image/svg/Instagram";
+import { Link } from "react-router-dom";
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className={classes.header}>
+        <h3 className={classes.h3}>Modern Studio</h3>
+        <div className={classes.action}>
+          <a href="/">
+            <Fb />
+          </a>
+          <a href="/">
+            <Instagram />
+          </a>
+        </div>
+      </div>
+      <ul className={classes.menu}>
+        <li>
+          <Link to="/offer">Oferty</Link>
+        </li>
+        <li>
+          <Link to="/services">Usługi</Link>
+        </li>
+        <li>
+          <Link to="/realization">Realizacje</Link>
+        </li>
+        <li>
+          <Link to="/contact">Kontakt</Link>
+        </li>
+        <li>
+          <Link to="/team">Zespół</Link>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
