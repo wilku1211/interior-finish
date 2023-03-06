e949feb46aeef4de1f1fe2e1e2370cf805bec163
wilku1211 <wilku1211c@gmail.com>
2023-03-06 10:32:07 +0100

    Services, contact  pages dextop improved

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,23 +3,29 @@ import Heading from "../components/UI/Heading/Heading";
 import classes from "./Contact.module.scss";
 import Tel from "../assets/image/svg/Tel";
 import ContactForm from "../components/ContactForm";
+import ContactImage from "../assets/image/contact/contact.jpg";
 
 const Contact = () => {
   return (
     <section className={classes.wrapper}>
-      <Heading title="Kontakt" isDecorated isFullHeight />
+      <div className={classes.content}>
+        <Heading title="Kontakt" isDecorated isFullHeight />
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo dicta
-        harum pariatur dignissimos nobis! Praesentium, veritatis..
-      </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo dicta
+          harum pariatur dignissimos nobis! Praesentium, veritatis..
+        </p>
 
-      <div className={classes.call}>
-        <Tel />
-        <span>Telefon: 777 888 999</span>
+        <div className={classes.call}>
+          <Tel />
+          <span>Telefon: 777 888 999</span>
+        </div>
+
+        <ContactForm />
       </div>
-
-      <ContactForm />
+      <div className={classes["image-wrapper"]}>
+        <img src={ContactImage} alt="ContactImage" />
+      </div>
     </section>
   );
 };
